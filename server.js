4a8c408481f8d3b99d734d06fabfcb5927ce28b1
mrsaqlain75 <saqlain9696382@gmail.com>
2024-09-20 10:20:27 +0500

    Fixing the issue bcrypt

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const nodemailer = require('nodemailer');
 
 const saltRounds = 10; 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'SADEVS_DB';
