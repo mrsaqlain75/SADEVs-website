897fb09106edd0633d75fd5f924ed535030a3270
mrsaqlain75 <saqlain9696382@gmail.com>
2024-10-04 23:35:41 +0500

    Dashboard issue resolved

--- server.js
+++ server.js
@@ -579,7 +579,7 @@ app.get('/fetch', async (req, res) => {
       res.json(users);
   } catch (error) {
       console.error('Error fetching users:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({message: 'Internal Server Error' });
   } finally {
       client.close();
   }
